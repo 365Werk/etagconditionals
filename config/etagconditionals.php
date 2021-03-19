a03c66bd30552fdebc4ad9064c17c998264f6056
Hergen Dillema <h.dillema@365werk.nl>
2021-03-19 11:30:36 +0100

    Added support for weak comparisons, so W/ can be stripped from etags
    - Added support for weak comparisons within IfMatch
    - Added support for weak comparisons within IfNoneMatch
    - Added configurations options for both so strong comparisons can be enabled optionally
    - Added tests for both IfMatch and IfNoneMatch

--- config/etagconditionals.php
+++ config/etagconditionals.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    //
+    "if_match_weak" => env('IF_MATCH_WEAK', true),
+    "if_none_match_weak" => env('IF_NONE_MATCH_WEAK', true),
 ];
--- tests/IfMatchTest.php
+++ tests/IfMatchTest.php
@@ -2,6 +2,7 @@
 
 namespace Werk365\EtagConditionals\Tests;
 
+use Illuminate\Support\Facades\Config;
 use Orchestra\Testbench\TestCase;
 use Werk365\EtagConditionals\Middleware\IfMatch;
 
@@ -34,6 +35,32 @@ class IfMatchTest extends TestCase
     public function patch_request_returns_412_if_none_matching_IfMatch()
     {
         $ifMatch = '"'.md5($this->response.'ifMatch').'"';
+        $response = $this->withHeaders([
+            'If-Match' => $ifMatch,
+        ])
+            ->patch('/_test/if-match');
+
+        $response->assertStatus(412);
+    }
+
+    /** @test */
+    public function patch_request_returns_200_if_matching_weaktag_when_weak_is_enabled_in_config()
+    {
+        Config::set('etagconditionals.if_match_weak', true);
+        $ifMatch = 'W/"'.md5($this->response).'"';
+        $response = $this->withHeaders([
+            'If-Match' => $ifMatch,
+        ])
+            ->patch('/_test/if-match');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function patch_request_returns_412_if_matching_weaktag_when_weak_is_disabled_in_config()
+    {
+        Config::set('etagconditionals.if_match_weak', false);
+        $ifMatch = 'W/"'.md5($this->response).'"';
         $response = $this->withHeaders([
             'If-Match' => $ifMatch,
         ])
